bc76395b9ef4fb87ef253507d6de2a90807088b1
avesk <averykushner@gmail.com>
2019-10-24 23:32:25 -0700

    gets client to write corrupted jpg data to file

--- video-stream/node-udp/node-client.js
+++ video-stream/node-udp/node-client.js
@@ -2,6 +2,7 @@
 const dgram = require("dgram");
 const client = dgram.createSocket("udp4");
 const utf8 = require("utf8");
+const fs = require("fs");
 
 const HOST = "0.0.0.0";
 const PORT = 5100;
@@ -10,7 +11,12 @@ const message = utf8.encode("get");
 
 client.on("message", (message, remote) => {
   console.log(remote.address + ":" + remote.port + " - ");
-  console.log(message);
+
+  let jpg = Buffer.from(message, "base64");
+  fs.writeFile(`${Math.random()}my-file.jpg`, jpg, err => {
+    if (err) throw err;
+    console.log("The binary data has been decoded and saved to my-file.png");
+  });
 });
 
 client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
