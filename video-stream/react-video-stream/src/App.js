7d23ebf74691ae22d43580cbbb0f302d679e743e
avesk <averykushner@gmail.com>
2019-10-31 18:34:03 -0700

    adding video stream react app

---
+++ video-stream/react-video-stream/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import WebSocket from "ws";
+
+function App() {
+  const [imgSrc, setSrc] = useState("");
+
+  useEffect(() => {
+    const WS_URL = "ws://localhost:3001";
+    const ws = new WebSocket(WS_URL);
+    ws.onopen = () => console.log(`Connected to ${WS_URL}`);
+    ws.onmessage = message => {
+      // set the base64 string to the src tag of the image
+      setSrc(message.data);
+    };
+  });
+
+  return (
+    <div className="App">
+      <img src={imgSrc} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
