36f481c33d9320dad9d472d928fc54a73796d4aa
Logan Kuzyk <logan@kuzyk.net>
2020-01-17 15:05:34 -0800

    Final VideoStream testers, works with threading

--- video-stream/client.py
+++ video-stream/client.py
@@ -21,27 +21,24 @@ frame_idx = 0
 while True:
     sent = sock.sendto("get".encode('utf-8'), server_address)
     data, server = sock.recvfrom(65507)
-    if len(data) == 4:
-        # This is a message error sent back by the server
-        if(data == "FAIL"):
-            continue
-    array = zlib.decompress(data, 0)
-    array = np.frombuffer(array, dtype=np.dtype('uint8'))
-    final = cv2.imdecode(array, 1)
-    cv2.imshow("Image", final)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("Asking the server to quit")
-        sock.sendto("quit".encode('utf-8'), server_address)
-        print("Quitting")
-        break
-    frame_idx += 1
+    if (data is not None):
+        array = zlib.decompress(data, 0)
+        array = np.frombuffer(array, dtype=np.dtype('uint8'))
+        final = cv2.imdecode(array, 1)
+        cv2.imshow("Image", final)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Asking the server to quit")
+            sock.sendto("quit".encode('utf-8'), server_address)
+            print("Quitting")
+            break
+        frame_idx += 1
 
-    if frame_idx == 30:
-        t1 = time.time()
-        sys.stdout.write(
-            '\r Framerate : {:.2f} frames/s.     '.format(30 / (t1 - t0)))
-        sys.stdout.flush()
-        t0 = t1
-        frame_idx = 0
+        if frame_idx == 30:
+            t1 = time.time()
+            sys.stdout.write(
+                '\r Framerate : {:.2f} frames/s.     '.format(30 / (t1 - t0)))
+            sys.stdout.flush()
+            t0 = t1
+            frame_idx = 0
 
 cv2.destroyAllWindows()
